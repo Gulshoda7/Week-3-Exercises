f06f9f92fd2b6f8651d021d814a9800fc1f4ba3f
Gulshoda7 <gulshodaqoziboyeva6@gmail.com>
2025-10-15 12:48:43 +0500

    total_calculator.py

--- problem 2/total_calculator.py
+++ problem 2/total_calculator.py
@@ -1,18 +1,11 @@
-# --- Example 5: The while Loop ---
-# print("\n--- T-Minus Countdown ---")
+print(f'--- Running Total Calculator ---')
+total = 0
+print(f'Enter numbers to add them up. Type `done` to see the total.')
+while True:
+    number = input('Enter a number or `done`: ')
+    if number == 'done':
+        break
+    total += float(number)
 
-# counter = 5
-
-# while counter > 0:
-#     print(f"{counter}...")
-#     # IMPORTANT: Update the loop variable to avoid an infinite loop.
-#     counter = counter - 1  # or counter -= 1
-
-# print("Blast off!")\
-
-counter = 16
-while counter > 0:
-    print(f'{counter}')
-    counter /= 2
-
-print('okay!')
+print(f'--- Final Calculation ---')
+print(f'The final sum of all numbers is: {total}')
